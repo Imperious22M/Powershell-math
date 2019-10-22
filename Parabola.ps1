a912b25d825ba37c1ed14a901f345bc850060adf
Imperious22M <45678466+Imperious22M@users.noreply.github.com>
2019-10-22 13:27:34 -0500

    Add files via upload

---
+++ Parabola.ps1
@@ -0,0 +1,56 @@
+﻿function NumIn($prompt){
+ $num = Read-Host -Prompt $prompt
+ if($num.IndexOf("/")-eq -1){
+ return [double]::Parse($num)
+ }else{
+   $numerator = $num.Substring(0,$num.IndexOf("/"))
+   Write-Host $numerator
+   $denominator = $num.Substring($num.IndexOf("/")+1)
+   Write-Host $denominator
+   $out = [double]::Parse($numerator)/[double]::Parse($denominator)
+   return $out
+
+ }
+
+}
+
+
+function Parabola($numtype,$nump,$numk,$numh){
+$realp = $nump/4
+$focy = $numk+$realp
+$Dir = $numk-$realp
+$x1 = ($numh+(2*$realp))
+$x2 = ($numh-(2*$realp))
+ if( $numtype -eq 1){
+ Write-Host "must be vertical parabola"
+ Write-Host "Value of P is: $realp"
+ Write-Host "The vertex is: ($h,$k)"
+ Write-Host "The Focus is: ($h,$focy)"
+ Write-Host "The Directrix is: y=$Dir"
+ Write-Host "The first point of the L.T. = ($x1,$focy)"
+ Write-Host "The last point of the L.T = ($x2,$focy)"
+ } else{
+ Write-Host "must be a horizontal parabola"
+  Write-Host "Value of P is: $realp"
+ Write-Host "The vertex is: ($h,$k)"
+ Write-Host "The Focus is: ("($numh+$realp)",$k)"
+ Write-Host "The Directrix is: x="($numh-$realp)
+ Write-Host "The first point of the L.T. =("($numh+$realp)","($numk+(2*$realp))")"
+ Write-Host "The last point of the L.T =("($numh+$realp)","($numk-(2*$realp))")"
+
+
+ } 
+
+}
+
+
+$type = NumIn -prompt "what type is it, 1) vertical 2) horizontal"
+$h = NumIn -prompt "enter h value" 
+$k = NumIn -prompt "enter k value"
+$p = NumIn -prompt "Enter p value"
+
+Parabola -numtype $type -nump $p -numk $k -numh  $h
+
+
+
+
---
+++ completingthesquare.ps1
@@ -0,0 +1,108 @@
+﻿function NumIn($prompt){
+ $num = Read-Host -Prompt $prompt
+ if($num.IndexOf("/")-eq -1){
+ return [double]::Parse($num)
+ }else{
+   $numerator = $num.Substring(0,$num.IndexOf("/"))
+   Write-Host $numerator
+   $denominator = $num.Substring($num.IndexOf("/")+1)
+   Write-Host $denominator
+   $out = [double]::Parse($numerator)/[double]::Parse($denominator)
+   return $out
+
+ }
+
+}
+
+
+function SqrComnum($sqrx,$justx,$sqry,$justy,$cons){
+Write-Host $test
+
+if($sqrx -cne 0 -and $sqry -cne 0){
+#x section for completing the square
+$newx = $justx/$sqrx
+Write-Host $newx
+$newxover2 = $newx/2
+$xbcom = [Math]::Pow(($newx/2),2)
+$xbnegcom = 0-$xbcom
+ Write-Host $sqrx"(x+$newxover2)^2+$xbnegcom)"
+ # x section for completing the square
+ 
+ #section for completing the square with y
+ $newy = $justy/$sqry
+Write-Host $newy
+$newyover2 = $newy/2
+$ybcom = [Math]::Pow(($newy/2),2)
+$ybnegcom = 0-$ybcom
+$ycomplete = "$sqry(y+$newyover2)^2+$ybnegcom)"
+ Write-Host $ycomplete
+ #section for completing the square with y
+
+ $xadd = $sqrx*$xbnegcom
+ $yadd = $sqry*$ybnegcom
+
+ $finaladd = $xadd+$yadd+$cons
+
+ Write-Host "$sqrx(x+$newxover2)^2+($xadd)+$sqry(y+$newyover2)^2+($yadd)+($cons)"
+
+ Write-Host "Final equation:"
+ $final =  "$sqrx(x+$newxover2)^2+$sqry(y+$newyover2)^2+($finaladd)=0"
+
+ Write-Host $final
+
+}elseif($sqrx -eq 0 -and $sqry -cne 0){
+$newy = $justy/$sqry
+Write-Host $newy
+$newyover2 = $newy/2
+$ybcom = [Math]::Pow(($newy/2),2)
+$ybnegcom = 0-$ybcom
+$ycomplete = "$sqry(y+$newyover2)^2+$ybnegcom)"
+ Write-Host $ycomplete
+ $yadd = $sqry*$ybnegcom
+ $finaladd = $yadd+$cons
+ Write-Host "Final equation:"
+  $final = "$sqry(y+$newyover2)^2+("+$justx+")x +($finaladd)=0"
+  Write-Host $final
+
+}elseif($sqry -eq 0 -and $sqrx -cne 0){
+
+$newx = $justx/$sqrx
+Write-Host $newx
+$newxover2 = $newx/2
+$xbcom = [Math]::Pow(($newx/2),2)
+$xbnegcom = 0-$xbcom
+ Write-Host $sqrx"(x+$newxover2)^2+$xbnegcom)"
+
+ $xadd = $sqrx*$xbnegcom
+ $finaladd = $xadd+$cons
+ Write-Host "Final equation:"
+  $final = "$sqrx(y+$newxover2)^2+("+$justy+")y +($finaladd)=0"
+  Write-Host $final
+
+
+}
+
+
+
+}
+
+$2x = Numin -prompt "enter the square x value"
+$x = Numin -prompt "enter the x value"
+$2y = Numin -prompt "enter the square y value"
+$y  = Numin -prompt "enter the y value"
+$c  = Numin -prompt "enter the constant"
+
+
+SqrComnum -sqrx $2x -justx $x -sqry $2y -justy $y -cons $c
+ 
+
+
+
+
+
+
+
+
+
+
+
